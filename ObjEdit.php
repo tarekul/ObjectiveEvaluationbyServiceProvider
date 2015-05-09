cbf1c0dfa2600d1ee9b4254f743aec355ec4c636
Tarekul Islam <bayridge4th@gmail.com>
2015-05-08 22:55:35 -0400

    Finished Project 90%
    Need to fix edit goals

--- ObjEdit.php
+++ ObjEdit.php
@@ -12,8 +12,491 @@
 	$username="root";
 	$password="root";
 	$databaseName="S1533608";
-
 	$connection=new mysqli($server,$username,$password,$databaseName);
+
+	
+	$SIDholder=0;
+	$OIDholder=0;
+	$rowSel1=0;
+	$rowSel2=0;
+	$rowSel3=0;
+	$rowSel4=0;
+	$go=0;
+	for($empVar2=1;$empVar2<31;$empVar2++){
+		for($empVar=1;$empVar<101;$empVar++){	
+			for($counter=1;$counter<5;$counter++){
+				//if(isset('pick'.$empVar2.'&'.$empVar.'&'.$counter)){
+				if($_POST['pick'.$empVar2.'&'.$empVar.'&'.$counter]=="asis"){
+					$go=1;
+					$SIDholder=$empVar2;
+					$OIDholder=$empVar;
+					if($counter==1){
+						$rowSel1=1;
+						//echo $rowSel1;
+					}
+					else if($counter==2)
+						$rowSel2=2;
+					else if($counter==3)
+						$rowSel3=3;
+					else if($counter==4)
+						$rowSel4=4;
+
+				}
+				//}
+				if($_POST['pick'.$empVar2.'&'.$empVar.'&'.$counter]=="updatemode"){
+					$go==1;
+					$SIDholder=$empVar2;
+					$OIDholder=$empVar;
+					if($counter==1){
+						$rowSel1=10;
+
+					}
+					else if($counter==2)
+						$rowSel2=20;
+					else if($counter==3)
+						$rowSel3=30;
+					else if($counter==4)
+						$rowSel4=40;
+
+				}
+
+				if($_POST['pick'.$empVar2.'&'.$empVar.'&'.$counter]=="del"){
+					$go==1;
+					$SIDholder=$empVar2;
+					$OIDholder=$empVar;
+					if($counter==1){
+						$rowSel1=100;
+
+					}
+					else if($counter==2)
+						$rowSel2=200;
+					else if($counter==3)
+						$rowSel3=300;
+					else if($counter==4)
+						$rowSel4=400;
+				}	
+
+			}
+		}
+	}
+	if($go==1){
+	$searchFor="SELECT * FROM list_of_services WHERE SID=$SIDholder";
+	$search=$connection->query($searchFor);
+	$fetch=mysqli_fetch_array($search);
+	$serviceName=$fetch['Service_Name'];
+	echo "<h1>Objective ".$OIDholder." of [".$SIDholder.":".$serviceName."]<h1>";	
+	
+	echo "<form action=lastedit.php method=post>";
+	echo "<table>";
+		echo "<tr>
+				<td>Measuring Instrument</td>
+				<td>Type</td>
+				<td>PDF DOCUMENTS</td>
+				<td>Below Expectations</td>
+				<td>Exceed Expectations</td>
+				<td>Meet Expectations</td>
+				<td><input type=submit name=editOID value=edit".$SIDholder."&".$OIDholder."></td>
+
+				
+			</tr>";	
+		echo "<tr>";
+			if($rowSel1==1){
+				$searchFor2="SELECT * FROM Grading WHERE OID=$OIDholder and RowNum=$rowSel1";
+				$search2=$connection->query($searchFor2);
+				$sql = "SELECT name FROM file WHERE OID=$OIDholder and RowNum=$rowSel1";
+				$result = $connection->query($sql);
+				$sql2="SELECT * FROM EvaluationCondition WHERE OID=$OIDholder and RowNum=$rowSel1";
+				$result2=$connection->query($sql2);
+				$fetch2=mysqli_fetch_array($search2);  
+				$row=mysqli_fetch_array($result);
+				$row2=mysqli_fetch_array($result2);
+				$MInstr=$fetch2['MeasuringInstrument'];
+				$type=$fetch2['Type'];
+				$pdf=$row['name'];
+				$Below=$row2['BelowCondition'];
+				$Exceed=$row2['ExceedCondition'];
+
+
+				echo "<td>Name: ".$MInstr."</td>"; 
+				echo "<td>type: ".$type."</td>";
+				echo "<td>".$pdf."</td>";
+				echo "<td>".$Below."</td>";
+				echo "<td>".$Exceed."</td>";
+				$Below++;
+				$Exceed--;
+				echo "<td>".$Below."% ~ ".$Exceed."%</td>";
+
+			
+			}
+			if($rowSel1==10){
+				$counter=$rowSel1=1;
+			    $empVar=$OIDholder;
+				$measureVar=$_POST['write'.$empVar.'&'.$counter];
+				//echo $measureVar;
+				$update="UPDATE Grading SET MeasuringInstrument='".$measureVar."' WHERE OID=$OIDholder and RowNum=$rowSel1";
+				$connection->query($update);
+				echo "<td>Name: ".$measureVar."</td>"; 
+				$typeChange=$_POST['check'.$empVar.'&'.$counter];
+				//echo $typeChange;
+				$update2="UPDATE Grading SET Type='".$typeChange."' WHERE OID=$OIDholder and RowNum=$rowSel1";
+				$connection->query($update2);
+				echo "<td>type: ".$typeChange."</td>";
+				
+				if(isset($_FILES['uploaded_file'.$empVar.'&'.$counter])){
+					$name = $connection->real_escape_string($_FILES['uploaded_file'.$empVar.'&'.$counter]['name']);
+        			$mime = $connection->real_escape_string($_FILES['uploaded_file'.$empVar.'&'.$counter]['type']);
+        			$data = $connection->real_escape_string(file_get_contents($_FILES  ['uploaded_file'.$empVar.'&'.$counter]['tmp_name']));
+        			$size = intval($_FILES['uploaded_file'.$empVar.'&'.$counter]['size']);
+
+        			$query="UPDATE file SET name='{$name}', mime='{$mime}', data='{$data}', size='{$size}' WHERE OID=$empVar AND RowNum=$rowSel1";
+        			//echo $query;
+        			$result = $connection->query($query);
+ 
+        			// Check if it was successfull
+        			if($result) {
+            			echo 'Success! Your file was successfully added!';
+        			}
+        			else {
+            			echo 'Error! Failed to insert the file';
+					}
+				}
+				$sql = "SELECT name FROM file WHERE OID=$OIDholder AND RowNum=$rowSel1";
+				$result = $connection->query($sql);
+				$row=mysqli_fetch_array($result);
+				$pdfName=$row['name'];
+				echo "<td>".$pdfName."</td>";
+
+				$Below=$_POST['myList'.$empVar.$counter];
+				$update3="UPDATE EvaluationCondition SET BelowCondition=$Below WHERE OID=$OIDholder and RowNum=$rowSel1";
+				$connection->query($update3);
+				echo "<td>~".$Below."</td>";
+
+				$Exceed=$_POST['myList2'.$empVar.$counter];
+				$update4="UPDATE EvaluationCondition SET ExceedCondition=$Exceed WHERE OID=$OIDholder and RowNum=$rowSel1";
+				$connection->query($update4);
+				echo "<td>~".$Exceed."</td>";
+				$Below++;
+				$Exceed--;
+				echo "<td>".$Below."~ ".$Exceed."</td>";
+
+			}
+
+			if($rowSel1==100){
+				$rowSel1=1;
+				$delete="DELETE FROM Grading WHERE OID=$OIDholder AND RowNum=$rowSel1";
+				//echo $delete;
+				$deleterow=$connection->query($delete);
+				$delete2="DELETE FROM EvaluationCondition WHERE OID=$OIDholder AND RowNum=$rowSel1";
+				$connection->query($delete2);
+				$delete3="DELETE FROM file WHERE OID=$OIDholder AND RowNum=$rowSel1";
+				$connection->query($delete3);
+			}
+
+
+		echo "</tr>";	
+
+		echo "<tr>";
+			if($rowSel2==2){
+				$searchFor2="SELECT * FROM Grading WHERE OID=$OIDholder and RowNum=$rowSel2";
+				$search2=$connection->query($searchFor2);
+				$sql = "SELECT name FROM file WHERE OID=$OIDholder and RowNum=$rowSel2";
+				$result = $connection->query($sql);
+				$sql2="SELECT * FROM EvaluationCondition WHERE OID=$OIDholder and RowNum=$rowSel2";
+				$result2=$connection->query($sql2);
+				$fetch2=mysqli_fetch_array($search2);  
+				$row=mysqli_fetch_array($result);
+				$row2=mysqli_fetch_array($result2);
+				$MInstr=$fetch2['MeasuringInstrument'];
+				$type=$fetch2['Type'];
+				$pdf=$row['name'];
+				$Below=$row2['BelowCondition'];
+				$Exceed=$row2['ExceedCondition'];
+
+
+				echo "<td>Name: ".$MInstr."</td>"; 
+				echo "<td>type: ".$type."</td>";
+				echo "<td>".$pdf."</td>";
+				echo "<td>".$Below."</td>";
+				echo "<td>".$Exceed."</td>";
+				$Below++;
+				$Exceed--;
+				echo "<td>".$Below."% ~ ".$Exceed."%</td>";
+
+			
+			}
+			if($rowSel2==20){
+				$counter=$rowSel2=2;
+			    $empVar=$OIDholder;
+				$measureVar=$_POST['write'.$empVar.'&'.$counter];
+				//echo $measureVar;
+				$update="UPDATE Grading SET MeasuringInstrument='".$measureVar."' WHERE OID=$OIDholder and RowNum=$rowSel2";
+				$connection->query($update);
+				echo "<td>Name: ".$measureVar."</td>"; 
+				$typeChange=$_POST['check'.$empVar.'&'.$counter];
+				//echo $typeChange;
+				$update2="UPDATE Grading SET Type='".$typeChange."' WHERE OID=$OIDholder and RowNum=$rowSel2";
+				$connection->query($update2);
+				echo "<td>type: ".$typeChange."</td>";
+				
+				if(isset($_FILES['uploaded_file'.$empVar.'&'.$counter])){
+					$name = $connection->real_escape_string($_FILES['uploaded_file'.$empVar.'&'.$counter]['name']);
+        			$mime = $connection->real_escape_string($_FILES['uploaded_file'.$empVar.'&'.$counter]['type']);
+        			$data = $connection->real_escape_string(file_get_contents($_FILES  ['uploaded_file'.$empVar.'&'.$counter]['tmp_name']));
+        			$size = intval($_FILES['uploaded_file'.$empVar.'&'.$counter]['size']);
+
+        			$query="UPDATE file SET name='{$name}', mime='{$mime}', data='{$data}', size='{$size}' WHERE OID=$empVar AND RowNum=$rowSel2";
+        			//echo $query;
+        			$result = $connection->query($query);
+ 
+        			// Check if it was successfull
+        			if($result) {
+            			echo 'Success! Your file was successfully added!';
+        			}
+        			else {
+            			echo 'Error! Failed to insert the file';
+					}
+				}
+				$sql = "SELECT name FROM file WHERE OID=$OIDholder AND RowNum=$rowSel2";
+				$result = $connection->query($sql);
+				$row=mysqli_fetch_array($result);
+				$pdfName=$row['name'];
+				echo "<td>".$pdfName."</td>";
+
+				$Below=$_POST['myList'.$empVar.$counter];
+				$update3="UPDATE EvaluationCondition SET BelowCondition=$Below WHERE OID=$OIDholder and RowNum=$rowSel2";
+				$connection->query($update3);
+				echo "<td>~".$Below."</td>";
+
+				$Exceed=$_POST['myList2'.$empVar.$counter];
+				$update4="UPDATE EvaluationCondition SET ExceedCondition=$Exceed WHERE OID=$OIDholder and RowNum=$rowSel2";
+				$connection->query($update4);
+				echo "<td>~".$Exceed."</td>";
+				$Below++;
+				$Exceed--;
+				echo "<td>".$Below."~ ".$Exceed."</td>";
+
+			}
+
+			if($rowSel2==200){
+				$rowSel2=2;
+				$delete="DELETE FROM Grading WHERE OID=$OIDholder AND RowNum=$rowSel2";
+				//echo $delete;
+				$deleterow=$connection->query($delete);
+				$delete2="DELETE FROM EvaluationCondition WHERE OID=$OIDholder AND RowNum=$rowSel2";
+				$connection->query($delete2);
+				$delete3="DELETE FROM file WHERE OID=$OIDholder AND RowNum=$rowSel2";
+				$connection->query($delete3);
+			}
+
+				
+
+		echo "</tr>";
+
+		echo "<tr>";
+			if($rowSel3==3){
+				$searchFor2="SELECT * FROM Grading WHERE OID=$OIDholder and RowNum=$rowSel3";
+				$search2=$connection->query($searchFor2);
+				$sql = "SELECT name FROM file WHERE OID=$OIDholder and RowNum=$rowSel3";
+				$result = $connection->query($sql);
+				$sql2="SELECT * FROM EvaluationCondition WHERE OID=$OIDholder and RowNum=$rowSel3";
+				$result2=$connection->query($sql2);
+				$fetch2=mysqli_fetch_array($search2);  
+				$row=mysqli_fetch_array($result);
+				$row2=mysqli_fetch_array($result2);
+				$MInstr=$fetch2['MeasuringInstrument'];
+				$type=$fetch2['Type'];
+				$pdf=$row['name'];
+				$Below=$row2['BelowCondition'];
+				$Exceed=$row2['ExceedCondition'];
+
+
+				echo "<td>Name: ".$MInstr."</td>"; 
+				echo "<td>type: ".$type."</td>";
+				echo "<td>".$pdf."</td>";
+				echo "<td>".$Below."</td>";
+				echo "<td>".$Exceed."</td>";
+				$Below++;
+				$Exceed--;
+				echo "<td>".$Below."% ~ ".$Exceed."%</td>";
+
+			
+			}
+			if($rowSel3==30){
+				$counter=$rowSel3=3;
+			    $empVar=$OIDholder;
+				$measureVar=$_POST['write'.$empVar.'&'.$counter];
+				//echo $measureVar;
+				$update="UPDATE Grading SET MeasuringInstrument='".$measureVar."' WHERE OID=$OIDholder and RowNum=$rowSel3";
+				$connection->query($update);
+				echo "<td>Name: ".$measureVar."</td>"; 
+				$typeChange=$_POST['check'.$empVar.'&'.$counter];
+				//echo $typeChange;
+				$update2="UPDATE Grading SET Type='".$typeChange."' WHERE OID=$OIDholder and RowNum=$rowSel3";
+				$connection->query($update2);
+				echo "<td>type: ".$typeChange."</td>";
+				
+				if(isset($_FILES['uploaded_file'.$empVar.'&'.$counter])){
+					$name = $connection->real_escape_string($_FILES['uploaded_file'.$empVar.'&'.$counter]['name']);
+        			$mime = $connection->real_escape_string($_FILES['uploaded_file'.$empVar.'&'.$counter]['type']);
+        			$data = $connection->real_escape_string(file_get_contents($_FILES  ['uploaded_file'.$empVar.'&'.$counter]['tmp_name']));
+        			$size = intval($_FILES['uploaded_file'.$empVar.'&'.$counter]['size']);
+
+        			$query="UPDATE file SET name='{$name}', mime='{$mime}', data='{$data}', size='{$size}' WHERE OID=$empVar AND RowNum=$rowSel3";
+        			//echo $query;
+        			$result = $connection->query($query);
+ 
+        			// Check if it was successfull
+        			if($result) {
+            			echo 'Success! Your file was successfully added!';
+        			}
+        			else {
+            			echo 'Error! Failed to insert the file';
+					}
+				}
+				$sql = "SELECT name FROM file WHERE OID=$OIDholder AND RowNum=$rowSel3";
+				$result = $connection->query($sql);
+				$row=mysqli_fetch_array($result);
+				$pdfName=$row['name'];
+				echo "<td>".$pdfName."</td>";
+
+				$Below=$_POST['myList'.$empVar.$counter];
+				$update3="UPDATE EvaluationCondition SET BelowCondition=$Below WHERE OID=$OIDholder and RowNum=$rowSel3";
+				$connection->query($update3);
+				echo "<td>~".$Below."</td>";
+
+				$Exceed=$_POST['myList2'.$empVar.$counter];
+				$update4="UPDATE EvaluationCondition SET ExceedCondition=$Exceed WHERE OID=$OIDholder and RowNum=$rowSel3";
+				$connection->query($update4);
+				echo "<td>~".$Exceed."</td>";
+				$Below++;
+				$Exceed--;
+				echo "<td>".$Below."~ ".$Exceed."</td>";
+
+			}
+
+			if($rowSel3==300){
+				$rowSel3=3;
+				$delete="DELETE FROM Grading WHERE OID=$OIDholder AND RowNum=$rowSel3";
+				//echo $delete;
+				$deleterow=$connection->query($delete);
+				$delete2="DELETE FROM EvaluationCondition WHERE OID=$OIDholder AND RowNum=$rowSel3";
+				$connection->query($delete2);
+				$delete3="DELETE FROM file WHERE OID=$OIDholder AND RowNum=$rowSel3";
+				$connection->query($delete3);
+			}
+
+				
+
+		echo "</tr>";
+
+		echo "<tr>";
+			if($rowSel4==4){
+				$searchFor2="SELECT * FROM Grading WHERE OID=$OIDholder and RowNum=$rowSel4";
+				$search2=$connection->query($searchFor2);
+				$sql = "SELECT name FROM file WHERE OID=$OIDholder and RowNum=$rowSel4";
+				$result = $connection->query($sql);
+				$sql2="SELECT * FROM EvaluationCondition WHERE OID=$OIDholder and RowNum=$rowSel4";
+				$result2=$connection->query($sql2);
+				$fetch2=mysqli_fetch_array($search2);  
+				$row=mysqli_fetch_array($result);
+				$row2=mysqli_fetch_array($result2);
+				$MInstr=$fetch2['MeasuringInstrument'];
+				$type=$fetch2['Type'];
+				$pdf=$row['name'];
+				$Below=$row2['BelowCondition'];
+				$Exceed=$row2['ExceedCondition'];
+
+
+				echo "<td>Name: ".$MInstr."</td>"; 
+				echo "<td>type: ".$type."</td>";
+				echo "<td>".$pdf."</td>";
+				echo "<td>".$Below."</td>";
+				echo "<td>".$Exceed."</td>";
+				$Below++;
+				$Exceed--;
+				echo "<td>".$Below."% ~ ".$Exceed."%</td>";
+
+			
+			}
+			if($rowSel4==40){
+				$counter=$rowSel4=4;
+			    $empVar=$OIDholder;
+				$measureVar=$_POST['write'.$empVar.'&'.$counter];
+				//echo $measureVar;
+				$update="UPDATE Grading SET MeasuringInstrument='".$measureVar."' WHERE OID=$OIDholder and RowNum=$rowSel4";
+				$connection->query($update);
+				echo "<td>Name: ".$measureVar."</td>"; 
+				$typeChange=$_POST['check'.$empVar.'&'.$counter];
+				//echo $typeChange;
+				$update2="UPDATE Grading SET Type='".$typeChange."' WHERE OID=$OIDholder and RowNum=$rowSel4";
+				$connection->query($update2);
+				echo "<td>type: ".$typeChange."</td>";
+				
+				if(isset($_FILES['uploaded_file'.$empVar.'&'.$counter])){
+					$name = $connection->real_escape_string($_FILES['uploaded_file'.$empVar.'&'.$counter]['name']);
+        			$mime = $connection->real_escape_string($_FILES['uploaded_file'.$empVar.'&'.$counter]['type']);
+        			$data = $connection->real_escape_string(file_get_contents($_FILES  ['uploaded_file'.$empVar.'&'.$counter]['tmp_name']));
+        			$size = intval($_FILES['uploaded_file'.$empVar.'&'.$counter]['size']);
+
+        			$query="UPDATE file SET name='{$name}', mime='{$mime}', data='{$data}', size='{$size}' WHERE OID=$empVar AND RowNum=$rowSel4";
+        			//echo $query;
+        			$result = $connection->query($query);
+ 
+        			// Check if it was successfull
+        			if($result) {
+            			echo 'Success! Your file was successfully added!';
+        			}
+        			else {
+            			echo 'Error! Failed to insert the file';
+					}
+				}
+				$sql = "SELECT name FROM file WHERE OID=$OIDholder AND RowNum=$rowSel4";
+				$result = $connection->query($sql);
+				$row=mysqli_fetch_array($result);
+				$pdfName=$row['name'];
+				echo "<td>".$pdfName."</td>";
+
+				$Below=$_POST['myList'.$empVar.$counter];
+				$update3="UPDATE EvaluationCondition SET BelowCondition=$Below WHERE OID=$OIDholder and RowNum=$rowSel4";
+				$connection->query($update3);
+				echo "<td>~".$Below."</td>";
+
+				$Exceed=$_POST['myList2'.$empVar.$counter];
+				$update4="UPDATE EvaluationCondition SET ExceedCondition=$Exceed WHERE OID=$OIDholder and RowNum=$rowSel4";
+				$connection->query($update4);
+				echo "<td>~".$Exceed."</td>";
+				$Below++;
+				$Exceed--;
+				echo "<td>".$Below."~ ".$Exceed."</td>";
+
+			}
+
+			if($rowSel4==400){
+				$rowSel4=4;
+				$delete="DELETE FROM Grading WHERE OID=$OIDholder AND RowNum=$rowSel4";
+				//echo $delete;
+				$deleterow=$connection->query($delete);
+				$delete2="DELETE FROM EvaluationCondition WHERE OID=$OIDholder AND RowNum=$rowSel4";
+				$connection->query($delete2);
+				$delete3="DELETE FROM file WHERE OID=$OIDholder AND RowNum=$rowSel4";
+				$connection->query($delete3);
+			}
+
+				
+
+		echo "</tr>";
+
+	echo "</table>";
+	echo "</form>";	
+	}
+	
+	
+	
+
+	
+	//this code starting from here will execute only when the link is 
+	//selected for the first time in index.php
+	if($go==0){
 	$SIDholder=$_GET['SerID']; //this is the SID of the selected link
 	$OIDholder=$_GET['ObjID']; //this is the link that was selected.
 	
@@ -44,7 +527,7 @@
 				<td>Below Expectations</td>
 				<td>Exceed Expectations</td>
 				<td>Meet Expectations</td>
-				<td><input type=submit name=editOID value=edit".$OIDholder."></td>
+				<td><input type=submit name=editOID value=edit".$SIDholder."&".$OIDholder."></td>
 
 				
 			</tr>";
@@ -53,35 +536,42 @@
 	//Need to fetch the contents of table
 	$searchFor2="SELECT * FROM Grading WHERE OID=$OIDholder";
 	$search2=$connection->query($searchFor2);
-	while($fetch2=mysqli_fetch_array($search2)){
+	$sql = "SELECT * FROM file WHERE OID=$OIDholder";
+	$result = $connection->query($sql);
+	$sql2="SELECT * FROM EvaluationCondition WHERE OID=$OIDholder";
+	$result2=$connection->query($sql2);
+	while($fetch2=mysqli_fetch_array($search2) AND $row=mysqli_fetch_array($result) AND $row2=mysqli_fetch_array($result2)){
 		$MInstr=$fetch2['MeasuringInstrument'];
 		$type=$fetch2['Type'];
-		$pdf=$fetch2['PDFdocument'];
-		$searchFor3="SELECT * FROM EvaluationCondition WHERE OID=$OIDholder";
-		$search3=$connection->query($searchFor3);
-		$fetch3=mysqli_fetch_array($search3);
-		$Bexp=$fetch3['BelowCondition'];
-		$Eexp=$fetch3['ExceedCondition'];
-		$BexpMeet=$Bexp+1;
-		$EexpMeet=$Eexp+1;
+		$pdfName=$row['name'];
+		$ID=$row['id'];
+		$Below=$row2['BelowCondition'];
+		$Exceed=$row2['ExceedCondition'];
 		echo "<tr>";
 				echo "<td>Name: ".$MInstr."</td>"; 
 				echo "<td>type: ".$type."</td>";
-				echo "<td>".$pdf."</td>"; 
-				echo "<td>~".$Bexp."%</td>";
-				echo "<td>~".$Eexp."%</td>";
-				echo "<td>".$BexpMeet."% ~ ".$EexpMeet."%</td>";
-
+				echo "<td><a href=get_file.php?id=".$ID.">".$pdfName."</a></td>";
+				//echo "<td>".$pdfName."</td>";
+				echo "<td>".$Below."</td>";
+				echo "<td>".$Exceed."</td>";
+				$Below++;
+				$Exceed--;
+				echo "<td>".$Below."~ ".$Exceed."</td>";
 				
-		
+				
+		echo "</tr>";
 		
 	}
 	
-	echo "</tr>";
+	
+		
+		
 	 
 	
 	echo "</table>";	
 	echo "</form>";	
+	}
+	
 	?>
 </body>
 </html>
--- lastedit.php
+++ lastedit.php
@@ -9,19 +9,23 @@
 <body>
 	<?php
 	$connection = new mysqli("localhost","root","root","S1533608");
-	//Need to store the OID from the ObjEdit page.
-	$empVar; //this will hold the OID.
-		for($i=1;$i<101;$i++){
-			if($_POST['editOID']=="edit".$i){
-				 $empVar=$i; 
-
-			}
-		}	
 	//Now the tables need to be created
 	//table headers first	
 
-	echo "<form action=ObjEdit.php method=post>";
+	echo "<form action=ObjEdit.php method=post enctype=multipart/form-data>";
 	echo "<table>";
+		//Need to store the OID from the ObjEdit page.
+	$empVar; //this will hold the OID.
+	$empVar2;//this will hold SID
+		for($i=1;$i<31;$i++){
+			for($p=1;$p<101;$p++){
+				if($_POST['editOID']=="edit".$i."&".$p){
+				 	$empVar2=$i;
+				 	$empVar=$p; 
+				 }
+
+			}
+		}
 		echo "<tr>
 				<td>Measuring Instrument</td>
 				<td>Type</td>
@@ -30,7 +34,7 @@
 				<td>Exceed Expectations</td>
 				
 				<td><input type=submit name=submit value=submit></td>
-
+				<td>Row#</td>
 				
 			</tr>";	
 
@@ -38,15 +42,15 @@
 	//Now I need to loop through the Grading table from database
 	//and print the contents on the table.		
 	$counter=1;		
-
+	
 	$searchFor2="SELECT * FROM Grading WHERE OID=$empVar";
 	$search2=$connection->query($searchFor2);
 	while($fetch2=mysqli_fetch_array($search2)){
-		$counter2=1;
-		$counter3=1;
+		
+		
 		$MInstr=$fetch2['MeasuringInstrument'];
 		$type=$fetch2['Type'];
-		$pdf=$fetch2['PDFdocument'];
+		
 		$searchFor3="SELECT * FROM EvaluationCondition WHERE OID=$empVar";
 		$search3=$connection->query($searchFor3);
 		$fetch3=mysqli_fetch_array($search3);
@@ -54,12 +58,18 @@
 		$Eexp=$fetch3['ExceedCondition'];
 		$BexpMeet=$Bexp+1;
 		$EexpMeet=$Eexp+1;
+
+		$search4="SELECT * FROM file WHERE OID=$empVar AND RowNum=$counter";
+		$searchfor4=$connection->query($search4);
+		$fetch4=mysqli_fetch_array($searchfor4);
+		$pdf=$fetch4['name']; 
+
 		echo "<tr>";
 			//echo "<td>".$empVar."</td>";
 				//textbox in each row with the name of task from
 				//database preset on textbox.
 				//user can edit the name.
-				echo "<td>Name:<input type=text>".$MInstr."</td>"; 
+				echo "<td>Name:<input type=text name=write".$empVar."&".$counter." value=".$MInstr."></td>"; 
 				/*need radio button to choose between 'assignment'
 				or 'exam'. Also need to check database and have atleast
 				one radio buttoned checked.*/
@@ -68,75 +78,70 @@
 
 				echo "<td>";
 				if($type=='assignment'){	
-					echo "<input type=radio name=check".$empVar.$counter." value=assignment checked>assignment";
-					echo "<input type=radio name=check".$empVar.$counter." value=exam>exam";
+					echo "<input type=radio name=check".$empVar."&".$counter." value=assignment checked>assignment";
+					echo "<input type=radio name=check".$empVar."&".$counter." value=exam>exam";
 				}
 				else{
-					echo "<input type=radio name=check".$empVar.$counter." value=assignment>assignment";
-					echo "<input type=radio name=check".$empVar.$counter." value=exam checked>exam";
+					echo "<input type=radio name=check".$empVar."&".$counter." value=assignment>assignment";
+					echo "<input type=radio name=check".$empVar."&".$counter." value=exam checked>exam";
 				}
 
 
 				echo "</td>";
-				$counter++;
-				echo "<td><input type=file name=uploaded>".$pdf."</td>"; 
+				
+				echo "<td><input type=file name=uploaded_file".$empVar."&".$counter."></td>"; 
+				
+ 
+    
+        
+ 
+
+				
 				echo "<td>";
-					echo"<select id = myList".$empVar.$counter.">";
-               		echo"<option value = 1 name=dropdwn".$empVar.$counter.$counter2.">10%</option>";
+					echo"<select name = myList".$empVar.$counter.">";
+               		echo"<option value = 10>10%</option>";
                		$counter2++;
-               		echo"<option value = 2 name=dropdwn".$empVar.$counter.$counter2.">20%</option>";
+               		echo"<option value = 20 >20%</option>";
                		$counter2++;
-               		echo"<option value = 3 name=dropdwn".$empVar.$counter.$counter2.">30%</option>";
+               		echo"<option value = 30 >30%</option>";
                		$counter2++;
-               		echo"<option value = 4 name=dropdwn".$empVar.$counter.$counter2.">40%</option>";
+               		echo"<option value = 40 >40%</option>";
              		echo "</select>";
 
 				echo "</td>";
 				echo "<td>";
-					echo"<select id = myList2".$empVar.$counter.">";
-               		echo"<option value = 1 name=dropdwn".$empVar.$counter.$counter3.">70%</option>";
+					echo"<select name = myList2".$empVar.$counter.">";
+               		echo"<option value = 70 >70%</option>";
                		$counter3++;
-               		echo"<option value = 2 name=dropdwn".$empVar.$counter.$counter3.">80%</option>";
+               		echo"<option value = 80 >80%</option>";
                		$counter3++;
-               		echo"<option value = 3 name=dropdwn".$empVar.$counter.$counter3.">90%</option>";
+               		echo"<option value = 90 >90%</option>";
                		$counter3++;
-               		echo"<option value = 4 name=dropdwn".$empVar.$counter.$counter3.">95%</option>";
+               		echo"<option value = 95 >95%</option>";
 
       				echo "</select>";
 
 				echo "</td>";
+				echo "<td>";
+					echo "<input type=radio name=pick".$empVar2."&".$empVar."&".$counter." value=asis checked>as-is";
+					
+					echo "<input type=radio name=pick".$empVar2."&".$empVar."&".$counter." value=updatemode>update";
+					
+					echo "<input type=radio name=pick".$empVar2."&".$empVar."&".$counter." value=del>delete";
 				
 
-				
+				echo "</td>";
+
+				echo"<td>".$counter."</td>";
 		
+		//Table made for user to edit. Now need to go back to ObjEdit page so changes appear.
 		echo "</tr>";
+		$counter++;
 	}
 	
-		
-
-		
-				
-
-					
-	
-
-
-
-
-
-
-
-
-
-
-
-
 	echo "</table>";
 	echo "</form>";
 
-
-
-
-
 	?>
 </body>
+</html>
